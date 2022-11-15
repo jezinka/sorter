345dd1f5906baa3d64185abf5e281ff16bd636d0
jezinka <jezina1989@gmail.com>
2022-11-15 08:53:39 +0100

    sorter main point

--- sorter.py
+++ sorter.py
@@ -1,34 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import time
-
-from adafruit_servokit import ServoKit  # https://circuitpython.readthedocs.io/projects/servokit/en/latest/
-
-# Parameters
-MIN_IMP = 500
-MAX_IMP = 2500
-MIN_ANG = 0
-MAX_ANG = 90
-
-# Objects
-pca = ServoKit(channels=16)
-servo = pca.servo[0]
+from dump import dump_left, dump_right
+from get_one import get_marble
 
 
-def main():
-    servo.set_pulse_width_range(MIN_IMP, MAX_IMP)
-
+def sort():
     while True:
-        for j in range(MIN_ANG, MAX_ANG, 1):
-            servo.angle = j
-            time.sleep(0.01)
-        for j in range(MAX_ANG, MIN_ANG, -1):
-            servo.angle = j
-            time.sleep(0.01)
-        servo.angle = None  # disable channel
-        time.sleep(0.5)
+        get_marble()
+        dump_left()
+        get_marble()
+        dump_right()
 
 
 if __name__ == '__main__':
-    main()
+    sort()
