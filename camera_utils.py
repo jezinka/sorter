66297f8355c38e0d2e6ce9b77f9a2972684ab5e4
jezinka <jezina1989@gmail.com>
2022-11-18 15:37:42 +0100

    picamera usage

---
+++ camera_utils.py
@@ -0,0 +1,14 @@
+import picamera
+import picamera.array
+
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as output:
+        camera.resolution = (1280, 720)
+        camera.capture(output, 'rgb')
+        print('Captured %dx%d image' % (
+            output.array.shape[1], output.array.shape[0]))
+        output.truncate(0)
+        camera.resolution = (640, 480)
+        camera.capture(output, 'rgb')
+        print('Captured %dx%d image' % (
+            output.array.shape[1], output.array.shape[0]))
